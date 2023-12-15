8b7accfc3a447662756e2efb4f7bc9b955d1de12
Home Winter <you@home-winter.com>
2023-12-15 18:55:57 -0400

    Crear la lista con sus operaciones, agregar, eliminar y mostrar una alerta de validacion

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,70 @@
 <script setup>
+import {ref} from "vue";
+
+let todoTitle = ref("");
+let arr = ref([]);
+let showAlert = ref(false);
+// const updateTitle = (e) => {
+//   todoTitle = e.target.value;
+// };
+
+const addTodo = () => {
+  if (todoTitle.value === "") {
+    showAlert.value = true;
+    return;
+  }
+  // arr.value = arr.value.concat([todoTitle.value]); // mutable
+  arr.value.push({
+    id: Math.floor(Math.random() * 1000),
+    title: todoTitle.value
+  }); // in mutable
+  todoTitle.value = "";
+};
+
+const removeTodo = (p_title) => {
+  // arr.value.splice(i, 1); // mutable
+  arr.value = arr.value.filter(p_todo => p_todo !== p_title); // in mutable
+};
+
+// export default {
+//   setup() {
+//     let todoTitle = ref("");
+//     const arr = ref([]);
+//
+//     const insertElement = () => {
+//       arr.value.push(todoTitle);
+//     };
+//
+//     return {
+//       todoTitle,
+//       arr,
+//       insertElement,
+//     };
+//   },
+// };
+
 </script>
 
 <template>
   <main class="container">
+    <div v-if="showAlert" class="alert">
+      <div>El titulo es necesario</div>
+      <div @click="showAlert = false" class="close-alert">&times;</div>
+    </div>
     <section>
       <form class="add-form">
-        <input type="text" placeholder="Escribe una lista por hacer">
+        <!--        <input v-on:input="updateTitle" type="text" placeholder="Escribe una lista por hacer">-->
+        <input v-model="todoTitle" type="text" placeholder="Escribe una lista por hacer">
         <div>
-          <button>Agregar</button>
+          <button @click.prevent="addTodo">Agregar</button>
         </div>
       </form>
     </section>
     <section>
-      <div class="todo">
-        <p>Pasear al Perro</p>
+      <div v-for="item in arr" v-bind:key="item.id" class="todo">
+        <p>{{ item.title }}</p>
         <div>
-          <button class="remove-todo">&times;</button>
+          <button @click="removeTodo(item)" class="remove-todo">&times;</button>
         </div>
       </div>
     </section>
@@ -23,6 +72,22 @@
 </template>
 
 <style scoped>
+.alert {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--danger-color);
+  margin-bottom: 20px;
+  padding: 0 20px 0 20px;
+  border-radius: 10px;
+  height: 50px;
+}
+
+.close-alert {
+  font-size: 50px;
+  cursor: pointer;
+}
+
 .add-form {
   display: flex;
   justify-content: space-between;
